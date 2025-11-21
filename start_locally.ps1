176ba6d38f1328d7beb4ecc5bf2efbea8f3ccbb7
abuomar80 <abuomar.omar@gmail.com>
2025-11-21 09:21:15 +0400

    1

---
+++ start_locally.ps1
@@ -0,0 +1,7 @@
+$env:KIOSKSERVICEPOINT = "test_service_point"
+$env:KIOSKFOLIOHOST = "http://localhost:9130"
+$env:KIOSKTENANT = "diku"
+$env:KIOSKTOKEN = "test_token"
+$env:KIOSKPORT = "3000"
+
+node app.js
